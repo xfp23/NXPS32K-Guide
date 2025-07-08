aa9ce8f01aa1eb03b15ba884d36e844636f40557
xfp23 <xfp57064@gmail.com>
2025-07-08 15:05:59 +0800

    创建指南

---
+++ example/S32KGuide/Sources/main.c
@@ -0,0 +1,95 @@
+/* ###################################################################
+**     Filename    : main.c
+**     Processor   : S32K1xx
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file main.c
+** @version 01.00
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup main_module main module documentation
+**  @{
+*/         
+/* MODULE main */
+
+
+/* Including necessary module. Cpu.h contains other modules needed for compiling.*/
+#include "Cpu.h"
+#include "pins_driver.h"
+
+
+  volatile int exit_code = 0;
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*! 
+  \brief The main function for the project.
+  \details The startup initialization sequence is the following:
+ * - startup asm routine
+ * - main()
+*/
+int main(void)
+{
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  #ifdef PEX_RTOS_INIT
+    PEX_RTOS_INIT();                   /* Initialization of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of Processor Expert internal initialization.                    ***/
+PINS_DRV_Init(NUM_OF_CONFIGURED_PINS, g_pin_mux_InitConfigArr);
+
+
+while(1)
+{
+PINS_DRV_WritePin(PTE,13,0); // 写入低
+PINS_DRV_WritePin(PTE,13,1);
+PINS_DRV_WritePins(PTE, 1 << 13); // 给端口整组写入掩
+	if((PINS_DRV_ReadPins(PTC) & 1 << 13) == 1) // 读取整个端口引脚
+	{
+		PINS_DRV_WritePin(PTE,13,1);
+	}
+
+//	if(PINS_DRV_ReadPin(PTC,13) == 0) // 读取单个引脚
+//	{
+//		PINS_DRV_WritePin(PTE,13,0);
+//	}
+
+}
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;) {
+    if(exit_code != 0) {
+      break;
+    }
+  }
+  return exit_code;
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END main */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.1 [05.21]
+**     for the NXP S32K series of microcontrollers.
+**
+** ###################################################################
+*/
